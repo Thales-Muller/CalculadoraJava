31f9dd0ad604746dd1d5649693e6d882d0c729bd
ThalesMuller <thalesmiller-tms@hotmail.com>
2019-04-29 21:48:20 -0300

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculadora	
+
+Projeto de uma calculadora em JAVASE
---
+++ src/Classe/main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classe;
+
+import View.JFrame_calculadora;
+
+/**
+ *
+ * @author Thales Muller
+ */
+public class main {
+    
+    public static void main(String args[]){
+        
+        new JFrame_calculadora ().setVisible(true);
+        
+    }
+    
+    
+}
